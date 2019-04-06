e84b5926ba7b875e9988dc3f527df4db1f7a71d4
MAURO ALEJANDRO HERNANDEZ DE LAS MUÑECAS <mauroahdlm@gmail.com>
2019-04-06 09:34:42 -0400

    commit mvc client creado

---
+++ MVCC2/MVCC2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCC2.MvcApplication" Language="C#" %>
---
+++ MVCC2/MVCC2/Models/Client.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCC2.Models
+{
+    public enum PayType
+    {
+        Cash,
+        CreditCard,
+        Check
+    }
+    public enum StatusType
+    {
+        Activo,
+        Inactivo
+    }
+    public class Client
+    {
+        [Key]
+        public int ClientId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public decimal Pounds { get; set; }
+        public int Age { get; set; }
+        public decimal Size { get; set; }
+        [Required]
+        public PayType Pay { get; set; }
+        public StatusType Status { get; set; }
+    }
+}
